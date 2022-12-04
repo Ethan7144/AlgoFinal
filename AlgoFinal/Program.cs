2b40d6389080ce139627726f040556c7ddfbf172
Ethan Tarlov <ejtarlov@me.com>
2022-12-04 13:36:19 -0800

    initial commit

---
+++ AlgoFinal/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+namespace AlgoFinal
+{
+    class Intractable
+    {
+        static int findMinbin(int[] weights, int maxWeight)
+        {
+          
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
+
